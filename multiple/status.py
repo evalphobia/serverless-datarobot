a7dbfa6757fafa4e512746fe1a304d263b4608d5
evalphobia <evalphobia@gmail.com>
2018-01-30 18:26:53 +0900

    Add status endpoint

---
+++ multiple/status.py
@@ -0,0 +1,17 @@
+import json
+
+BODY = {
+    "error": None,
+    "code": 200,
+    "status": "ok",
+}
+
+RESPONSE = {
+    "statusCode": 200,
+    "body": json.dumps(BODY)
+}
+
+
+def status(event, context):
+    """Return status."""
+    return RESPONSE
